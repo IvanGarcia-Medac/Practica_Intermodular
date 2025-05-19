35efe655d03a52336abc9e630b7a26e33a149855
ivan <igf0015@alu.medac.es>
2025-05-19 14:30:56 +0200

    Proyecto de java iniciado

---
+++ Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Proyecto_Intermodular.java
@@ -0,0 +1,21 @@
+
+
+package com.mycompany.proyecto_intermodular;
+
+
+public class Proyecto_Intermodular {
+
+    public static void main(String[] args) {
+        
+        int opcion=0;
+                
+               System.out.println("------------MENU------------"); 
+               System.out.println("¿QUE QUIERES HACER?"); 
+               System.out.println("1-VER TODOS LOS AGENTES"); 
+               System.out.println("2-VER ROLES"); 
+                
+                
+                
+                
+    }
+}
