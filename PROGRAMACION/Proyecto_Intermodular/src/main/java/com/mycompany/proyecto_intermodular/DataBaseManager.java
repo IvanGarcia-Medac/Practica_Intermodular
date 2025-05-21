47e6ea3c340964afd12a73de6cd08f5c27cc3fb6
ivan <igf0015@alu.medac.es>
2025-05-21 10:58:11 +0200

    main actualizado con menu y clase database iniciada

--- PROGRAMACION/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/DataBaseManager.java
+++ PROGRAMACION/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/DataBaseManager.java
@@ -1,15 +1,46 @@
 
 package com.mycompany.proyecto_intermodular;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataBaseManager {
   
+    //atributos
+    String url;  // localizacion de mi base de datos 
+    String user; // usuario
+    String password; // contraseña de mi base de datos
+    Connection conn;
     
-    private List<Agente> entidadesPrincipales;
-    private List<EntidadSecundaria> entidadesSecundarias;
 
-        Agente = new ArrayList<>();
-        EntidadSecundaria = new ArrayList<>();
+    //constructor 
+    public DataBaseManager() {
+
+        try {
+            this.url = "jdbc:mysql://localhost:3306/Valorant";
+            this.user = "VAL";
+            this.password = "MEDAC";
+            this.conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Error " + e.getMessage());
+        }
+    
+        
+        
+     
 }
+    public void cargarDatos(){
+            ArrayList<Agente>agentes = new ArrayList<>();
+            ArrayList<Roles>agentes = new ArrayList<>();
+            ArrayList<habilidad>agentes = new ArrayList<>();
+            
+            
+        }
+    
+    
+    
+}
+
--- PROGRAMACION/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Proyecto_Intermodular.java
+++ PROGRAMACION/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Proyecto_Intermodular.java
@@ -2,19 +2,24 @@
 
 package com.mycompany.proyecto_intermodular;
 
+import java.util.Scanner;
+
 
 public class Proyecto_Intermodular {
 
     public static void main(String[] args) {
         
-        int opcion=0;
+        int opcion=0;// CONTROLAR OPCIONE DEL USUARIO
+        
+        Scanner sc=new Scanner(System.in);
                 
                System.out.println("------------MENU------------"); 
                System.out.println("¿QUE QUIERES HACER?"); 
                System.out.println("1-VER TODOS LOS AGENTES"); 
                System.out.println("2-VER AGENTE CONCREO"); 
-               System.out.println("2-VER AGENTE CONCREO"); 
-                
+               System.out.println("3-VER ROLES Y HABILIDADES"); 
+               
+               opcion=Integer.parseInt(sc.nextLine());
                 
          switch (opcion) {
             case "1":
