a99f4ed7357456559a2276d12fec6ad07b6d7af9
Fernando Garcia <fgf0013@alu.medac.es>
2025-05-21 13:53:35 +0200

    he quitado el idRol

--- PROGRAMACION/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Rol.java
+++ PROGRAMACION/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Rol.java
@@ -4,21 +4,15 @@ package com.mycompany.proyecto_intermodular;
 
 public class Rol {
   
-    private int idRol;
     private String nombre;
     private String posicion;
 
     //Constructor
-    public Rol(int idRol, String nombre, String posicion) {
-        this.idRol = idRol;
+    public Rol(String nombre, String posicion) {
         this.nombre = nombre;
         this.posicion = posicion;
     }
     // Getters
-    public int getIdRol() {
-        return idRol;
-    }
-
     public String getNombre() {
         return nombre;
     }
@@ -28,10 +22,6 @@ public class Rol {
     }
 
     // Setters
-    public void setIdRol(int idRol) {
-        this.idRol = idRol;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -43,8 +33,9 @@ public class Rol {
     // toString
     @Override
     public String toString() {
-        return "Rol{" + "idRol=" + idRol + ", nombre=" + nombre + ", posicion=" + posicion + '}';
+        return "Rol{" + "nombre=" + nombre + ", posicion=" + posicion + '}';
     }
+    
     
     
     
